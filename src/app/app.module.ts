49750fe0adca6bf5745a88dd46cbbc969154e3f1
Azumist <lukaszszkwarek96@gmail.com>
2021-08-06 00:26:29 +0200

    created recipes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,18 +5,13 @@ import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { RecipesComponent } from './recipes/recipes.component';
-import { RecipeListComponent } from './recipes/recipe-list/recipe-list.component';
-import { RecipeItemComponent } from './recipes/recipe-list/recipe-item/recipe-item.component';
-import { RecipeDetailComponent } from './recipes/recipe-detail/recipe-detail.component';
-import { RecipeStartComponent } from './recipes/recipe-start/recipe-start.component';
-import { RecipeEditComponent } from './recipes/recipe-edit/recipe-edit.component';
 import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 import { ShoppingEditComponent } from './shopping-list/shopping-edit/shopping-edit.component';
 import { AuthComponent } from './auth/auth.component';
 import { AlertComponent } from './shared/alert/alert.component';
 
 import { AppRoutingModule } from './app-routing.module';
+import { RecipesModule } from './recipes/recipes.module';
 
 import { RecipeService } from './recipes/recipe.service';
 import { ShoppingListService } from './shopping-list/shopping-list.service';
@@ -32,18 +27,12 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
   declarations: [
     AppComponent,
     HeaderComponent,
-    RecipesComponent,
-    RecipeListComponent,
-    RecipeDetailComponent,
-    RecipeItemComponent,
     ShoppingListComponent,
     ShoppingEditComponent,
-    DropdownDirective,
-    RecipeStartComponent,
-    RecipeEditComponent,
+    // DropdownDirective,
     AlertComponent,
     AuthComponent,
-    ShortenPipe,
+    // ShortenPipe,
     LoadingSpinnerComponent,
   ],
   imports: [
@@ -52,6 +41,7 @@ import { LoadingSpinnerComponent } from './shared/loading-spinner/loading-spinne
     ReactiveFormsModule,
     HttpClientModule,
     AppRoutingModule,
+    RecipesModule
   ],
   providers: [
     ShoppingListService,
---
+++ src/app/recipes/recipes.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { RouterModule } from "@angular/router";
+import { ReactiveFormsModule } from "@angular/forms";
+
+import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
+import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
+import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.component";
+import { RecipeListComponent } from "./recipe-list/recipe-list.component";
+import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
+import { RecipesComponent } from "./recipes.component";
+
+import { ShortenPipe } from "../shared/shorten.pipe";
+import { DropdownDirective } from "../shared/dropdown.directive";
+
+@NgModule({
+  declarations: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+    DropdownDirective,
+    ShortenPipe,
+  ],
+  imports: [
+    RouterModule,
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  exports: [
+    RecipesComponent,
+    RecipeListComponent,
+    RecipeDetailComponent,
+    RecipeItemComponent,
+    RecipeStartComponent,
+    RecipeEditComponent,
+    DropdownDirective,
+  ]
+})
+export class RecipesModule {
+
+}
