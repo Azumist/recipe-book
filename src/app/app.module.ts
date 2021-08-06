8da77651ed55330a2e07bac24b1152b9b6111381
Azumist <lukaszszkwarek96@gmail.com>
2021-08-06 23:15:11 +0200

    added lazy loading of recipes module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,11 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import { AuthComponent } from './auth/auth.component';
 
 import { AppRoutingModule } from './app-routing.module';
-import { RecipesModule } from './recipes/recipes.module';
 import { SharedModule } from './shared/shared.module';
 
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
@@ -26,7 +23,7 @@ import { AuthModule } from './auth/auth.module';
     HttpClientModule,
     AppRoutingModule,
     SharedModule,
-    RecipesModule,
+    // RecipesModule, (is now lazy loaded)
     ShoppingListModule,
     AuthModule,
     CoreModule,
